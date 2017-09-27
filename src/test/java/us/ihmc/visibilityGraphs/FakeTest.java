00ee2a7c70360c21127f31d4348eb12871632c97
Sylvain Bertrand <sbertrand@ihmc.us>
2017-09-27 12:24:44 -0500

    Setting up bamboo plan for this repo.

---
+++ src/test/java/us/ihmc/visibilityGraphs/FakeTest.java
@@ -0,0 +1,14 @@
+package us.ihmc.visibilityGraphs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FakeTest
+{
+   @Test
+   public void testPassing() throws Exception
+   {
+      assertTrue(true);
+   }
+}
