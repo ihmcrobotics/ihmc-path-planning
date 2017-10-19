498d0188401edfdd52e47bd5ac75773ff9e54e73
gwiedebach <gwiedebach@ihmc.us>
2017-10-19 14:07:06 -0500

    Added tools class for planar regsions. Needs tests.

---
+++ src/main/java/us/ihmc/pathPlanning/visibilityGraphs/PlanarRegionTools.java
@@ -0,0 +1,142 @@
+package us.ihmc.pathPlanning.visibilityGraphs;
+
+import us.ihmc.euclid.geometry.ConvexPolygon2D;
+import us.ihmc.euclid.geometry.Line3D;
+import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
+import us.ihmc.euclid.transform.RigidBodyTransform;
+import us.ihmc.euclid.tuple2D.Point2D;
+import us.ihmc.euclid.tuple3D.Point3D;
+import us.ihmc.euclid.tuple3D.Vector3D;
+import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
+import us.ihmc.robotics.geometry.PlanarRegion;
+import us.ihmc.robotics.geometry.PlanarRegionsList;
+
+public class PlanarRegionTools
+{
+   /**
+    * Finds and returns the closest point the the provided point on the planar regions.
+    */
+  public static Point3D projectPointToPlanes(Point3DReadOnly point, PlanarRegionsList regions)
+  {
+     double smallestDistance = Double.POSITIVE_INFINITY;
+     Point3D closestPoint = null;
+
+     for (PlanarRegion region : regions.getPlanarRegionsAsList())
+     {
+        Point3D intersection = closestPointOnPlane(point, region);
+        double distance = intersection.distance(point);
+
+        if (closestPoint == null || distance < smallestDistance)
+        {
+           smallestDistance = distance;
+           closestPoint = intersection;
+        }
+     }
+
+     return closestPoint;
+  }
+
+   /**
+    * Projects the given point onto the planar region, returning the closest point on
+    * the region to the provided point.
+    */
+   public static Point3D closestPointOnPlane(Point3DReadOnly point, PlanarRegion region)
+   {
+      RigidBodyTransform regionToWorld = new RigidBodyTransform();
+      region.getTransformToWorld(regionToWorld);
+
+      Vector3D planeNormal = new Vector3D(0.0, 0.0, 1.0);
+      planeNormal.applyTransform(regionToWorld);
+
+      Point3D pointOnPlane = new Point3D();
+      pointOnPlane.set(region.getConvexPolygon(0).getVertex(0));
+      pointOnPlane.applyTransform(regionToWorld);
+
+      Point3D intersectionWithPlane = EuclidGeometryTools.intersectionBetweenLine3DAndPlane3D(pointOnPlane, planeNormal, point, planeNormal);
+      if (intersectionWithPlane == null)
+      {
+         return null;
+      }
+
+      Point3D intersectionInPlaneFrame = new Point3D(intersectionWithPlane);
+      intersectionInPlaneFrame.applyInverseTransform(regionToWorld);
+      Point2D intersectionInPlaneFrame2D = new Point2D(intersectionInPlaneFrame);
+
+      // checking convex hull here - might be better to check all polygons to avoid false positive
+      ConvexPolygon2D convexHull = region.getConvexHull();
+      if (!convexHull.isPointInside(intersectionInPlaneFrame2D))
+      {
+         convexHull.orthogonalProjection(intersectionInPlaneFrame2D);
+         intersectionWithPlane.setToZero();
+         intersectionWithPlane.set(intersectionInPlaneFrame2D);
+         intersectionWithPlane.applyTransform(regionToWorld);
+      }
+      return intersectionWithPlane;
+   }
+
+   /**
+    * Projects the given point onto a planar region from the list. The projection is done along
+    * the z axis in world frame and if there is multiple regions that the point can be
+    * projected onto, the highest intersection point will be returned.
+    * <p>
+    * Will return null if the is no planar region above or below the point.
+    */
+   public static Point3D projectPointToPlanesVertically(Point3DReadOnly point, PlanarRegionsList regions)
+   {
+      Line3D projectionLine = new Line3D(point, new Vector3D(0.0, 0.0, 1.0));
+      Point3D highestIntersection = null;
+
+      for (PlanarRegion region : regions.getPlanarRegionsAsList())
+      {
+         Point3D intersection = intersectRegionWithLine(region, projectionLine);
+
+         if (intersection == null)
+         {
+            continue;
+         }
+
+         if (highestIntersection == null || highestIntersection.getZ() < intersection.getZ())
+         {
+            highestIntersection = intersection;
+         }
+      }
+
+      return highestIntersection;
+   }
+
+   /**
+    * Will return the intersection point between a line and a single planar region. If
+    * the line does not intersect the region this method will return null.
+    */
+   public static Point3D intersectRegionWithLine(PlanarRegion region, Line3D projectionLine)
+   {
+      RigidBodyTransform regionToWorld = new RigidBodyTransform();
+      region.getTransformToWorld(regionToWorld);
+
+      Vector3D planeNormal = new Vector3D(0.0, 0.0, 1.0);
+      planeNormal.applyTransform(regionToWorld);
+
+      Point3D pointOnPlane = new Point3D();
+      pointOnPlane.set(region.getConvexPolygon(0).getVertex(0));
+      pointOnPlane.applyTransform(regionToWorld);
+
+      Point3DReadOnly pointOnLine = projectionLine.getPoint();
+      Vector3DReadOnly directionOfLine = projectionLine.getDirection();
+      Point3D intersectionWithPlane = EuclidGeometryTools.intersectionBetweenLine3DAndPlane3D(pointOnPlane, planeNormal, pointOnLine, directionOfLine);
+      if (intersectionWithPlane == null)
+      {
+         return null;
+      }
+
+      Point3D intersectionInPlaneFrame = new Point3D(intersectionWithPlane);
+      intersectionInPlaneFrame.applyInverseTransform(regionToWorld);
+      // checking convex hull here - might be better to check all polygons to avoid false positive
+      if (region.getConvexHull().isPointInside(intersectionInPlaneFrame.getX(), intersectionInPlaneFrame.getY()))
+      {
+         return intersectionWithPlane;
+      }
+
+      return null;
+   }
+}
