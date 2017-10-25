4d32d7ca2c8cbb36b67c78edea11bb60e42f8c54
Sylvain Bertrand <sbertrand@ihmc.us>
2017-10-24 20:26:38 -0500

    Formatted.

--- src/main/java/us/ihmc/pathPlanning/tools/AutomaticClusteringTool.java
+++ src/main/java/us/ihmc/pathPlanning/tools/AutomaticClusteringTool.java
@@ -1,7 +1,5 @@
 package us.ihmc.pathPlanning.tools;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,50 +38,48 @@ public class AutomaticClusteringTool
 
          }
       }
-      
-      
 
       System.out.println("Calculated a total of " + listOfClusters.size() + " clusters");
-//
-//      for (int i = 0; i < listOfClusters.size(); i++)
-//      {
-//         System.out.println("     Cluster - " + i + " has size of " + listOfClusters.get(i).getPointsInCluster().size());
-//      }
+      //
+      //      for (int i = 0; i < listOfClusters.size(); i++)
+      //      {
+      //         System.out.println("     Cluster - " + i + " has size of " + listOfClusters.get(i).getPointsInCluster().size());
+      //      }
 
       return listOfClusters;
    }
-     
+
    public static List<Cluster> createClusters2(List<Point3D> listOfPoints, double clusteringDistanceThreshold)
    {
       ArrayList<Cluster> listOfClusters = new ArrayList<>();
-      
+
       Cluster cluster = new Cluster();
-      
-      for(int i = 0; i < listOfPoints.size() - 1; i++)
+
+      for (int i = 0; i < listOfPoints.size() - 1; i++)
       {
          cluster.addRawPoint(listOfPoints.get(i));
-         
-         if(listOfPoints.get(i).distance(listOfPoints.get(i+1)) > 1.0)
+
+         if (listOfPoints.get(i).distance(listOfPoints.get(i + 1)) > 1.0)
          {
-            if(cluster.getRawPointsInCluster().size() > 1)
+            if (cluster.getRawPointsInCluster().size() > 1)
             {
                listOfClusters.add(cluster);
             }
             cluster = new Cluster();
          }
-         
-         if(i == listOfPoints.size() - 2)
+
+         if (i == listOfPoints.size() - 2)
          {
             cluster.addRawPoint(listOfPoints.get(listOfPoints.size() - 1));
-            
-            if(cluster.getRawPointsInCluster().size() > 1)
+
+            if (cluster.getRawPointsInCluster().size() > 1)
             {
                listOfClusters.add(cluster);
             }
 
          }
       }
-      
+
       System.out.println("Calculated a total of " + listOfClusters.size() + " clusters");
 
       return listOfClusters;
